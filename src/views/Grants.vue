79a477f7ab0ee6c7ae6436264a5aea30b2bc6828
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-05-31 10:51:54 -0400

    Remove sections for now.

--- src/views/Grants.vue
+++ src/views/Grants.vue
@@ -23,21 +23,21 @@
 			</div> 
 		</section>
 
-		<section class="grants-newsletter">
+		<!-- <section class="grants-newsletter">
 			<div class="container">
 				<p>Want to stay up-to-date on Park People news?</p>
 				<a class="button" href="http://parkpeople.us2.list-manage.com/subscribe?u=ba963c8c64482c0ad756245c3&id=efc9b053b8" target="_blank">Get the Park People newsletter!</a>
 			</div>
-		</section>
+		</section> -->
 
-		<section class="grant-sponsors">
+		<!-- <section class="grant-sponsors">
 			<p>Made possible by a great collaboration:</p>
 			<ul>
 				<li v-for="sponsor in data.meta_box._page_grant_sponsors" :key="sponsor['_page_g_sponsor_img']">
 					<a :href="sponsor['_page_g_sponsor_link']" target="_blank"><img :src="sponsor['_page_g_sponsor_img']" alt="logo"></a>
 				</li>
 			</ul>
-		</section>
+		</section> -->
 		
 		<!-- <app-related :title="data.meta_box._page_grant_resource_heading" :copy="data.meta_box._page_grant_resource_copy" :posts="relatedPosts"></app-related> -->
 	</div>
