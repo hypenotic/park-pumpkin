7b4f2d9c9be436e90cafc0b9e80bfa1715592bc7
Hubert <hubhcsi@gmail.com>
2018-06-08 14:23:33 -0400

    add layout to vue template

--- src/App.vue
+++ src/App.vue
@@ -1,355 +1,451 @@
 <template>
-    <div style="position: relative;">
-        <app-header></app-header>
-        <transition name="fade">
-            <router-view :key="$route.fullPath"></router-view>
-        </transition>
-        <app-footer></app-footer>
-        <div class="credits">
-            <div class="container">
-                <p>Made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://hypenotic.com" target="_blank">Hypenotic</a>.</p>
+    <div id="app">
+        <nav>
+            <div class="navinner" id="ni1">
+                <div id="home">
+                    <a href="#">
+                        <img src="src/assets/pplogo.png">
+                    </a>
+                </div>
+                <div id="social">
+                    ig t f
+                </div>
             </div>
-        </div>
+            <div class="navinner" id="ni2">
+                <div id="lang">
+                    <span class="sel">En</span><span>Fr</span>
+                </div>
+                <a href="" id="register" class="but">
+                            Create/register a group
+            </a>
+                <div id="logo">
+                    <a href="http://parkpeople.ca">
+                        <img src="src/assets/parkpeople.png">                
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <section id="banner" class="full">
+        </section>
+        <img id="moon" src="src/assets/moon.png">
+        <section id="hero" class="full">
+            <div class="fi">
+                <div class="blurb">
+                    <p>
+                        <span v-html="page.meta_box._page_phero_heading"></span>
+                    </p>
+                    <p>
+                        <span v-html="page.meta_box._page_phero_copy"></span>
+                    </p>
+                </div>
+                <div>
+                    <form class="herosearch">
+                        <input type="text" name="findnear" placeholder="Find a parade near you">
+                        <input type="submit" value="🔍︎">
+                    </form>
+                </div>
+            </div>
+        </section>
+        <app-map></app-map>
+        <section id="media" class="col-2">
+            <div>
+                <video controls>
+                    <source :src="page.meta_box._page_pmedia_video" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div>
+                <img :src="page.meta_box._page_pmedia_image">
+            </div>
+        </section>
+        <section id="howto" class="full middle">
+            <div class="heading center">
+                Simple Steps to Planning Your Pumpkin Parade
+            </div>
+            <div class="copy">
+                there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently, there is more text here apparently,
+            </div>
+        </section>
+        <section id="tips" class="full middle">
+            <div class="heading center">
+                THis is a heading
+            </div>
+            <div class="center">
+                some subtext
+            </div>
+            <div class="col-3">
+                <div>
+                    nice
+                </div>
+                <div>
+                    cool
+                </div>
+                <div>
+                    thing
+                </div>
+            </div>
+        </section>
+        <section id="partners">
+            <div>
+                sponsored by people
+            </div>
+            <div>
+                torono
+            </div>
+            <div>
+                metro
+            </div>
+        </section>
+        <section id="promo" class="col-3">
+            <div>
+                nice
+            </div>
+            <div>
+                cool
+            </div>
+            <div>
+                thing
+            </div>
+        </section>
+        <section id="cta">
+            click a shiny button
+        </section>
+        <section id="footer" class="col-2">
+            <div>
+                cool picture
+            </div>
+            <div>
+                contact info
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import Footer from './components/Footer.vue';
-    import { mapState } from 'vuex';
+    import store from './store';
+    import Map from './components/map/Map.vue';
+
     export default {
-    components: {
-        appHeader: Header,
-        appFooter: Footer
-    },
-    mounted: function() {
-        this.$store.dispatch("getLocations", {'type': 'initial-load'});
-        this.$store.dispatch("getActivities", {'type': 'initial-load'});
-    },
-    created: function() {
-        
-    },
-}
+        components: {
+            appMap: Map
+        },
+        store,
+        data(){
+            return {
+            }
+        },
+        computed:{
+            count(){
+                return store.state.count
+            },
+            page(){
+                return store.state.page
+            }
+        },
+        created() {
+            this.$store.dispatch('GET_PAGE');
+        },
+        methods: {
+            increment () {
+                store.commit('increment')
+            },
+            decrement () {
+                store.commit('decrement')
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
+    
 
-// Import Bulma's core
-@import "~bulma/sass/utilities/_all";
-@import url('https://fonts.googleapis.com/css?family=Dosis:400,700,800|Comfortaa|Merriweather');
-// Import Variables
-@import './styles/variables.scss';
-@import './styles/animate.scss';
+    /* ----- resets */
 
-$family-cursive: "Comfortaa", cursive;
-$family-sanserif: "Dosis", sans-serif;
-$serif: "Merriweather", serif;
-
-html, body {
-    font-size: 16px;
-    line-height: 1.5;
-    font-family: $family-cursive;
-    color: $body-color;
-    @media #{$medium-and-up} {
-		font-size: 18px;
-        line-height: 26px;
+    *{
+        box-sizing: border-box;
     }
-}
 
-.credits {
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    p {
-        margin: 0;
-        font-size: 0.7rem;
-        color: $white;
-        text-align: right;
-        padding: 16px;
-        opacity: 0.4;
-        a {
-            color: $white;
-            // text-decoration: underline;
-            border-bottom: 1px solid $white;
+    html, body{
+        margin:0;
+        padding:0;
+        border:0;
+    }
+    
+    html, body, #app{
+        height: 100%;
+    }
+
+    /* ----- font business and setup */
+
+    @media (orientation:landscape){
+        *{
+
+        }
+        html{
+            font-size: calc(0.9vw);
+        }
+        .col-2{
+            display: grid;
+            grid-template-columns: [left] 50% [right] 50% [end] 0;
+        }
+
+        .col-3{
+            display: grid;
+            grid-template-columns: [lg] auto [left] 25% [mid] 25% [right] 25% [rg] auto [end] 0;
+        }
+
+        .full{
+            height: 100%;
         }
     }
-}
 
-a {
-    color: $blue;
-}
+    @media (orientation:portrait){
+        *{
 
-p {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    margin-bottom: 1.5rem;
-    color: $body-color;
-}
+        }
+        html{
+            font-size: calc(0.7vw + 4pt);
+        }
+        .full{
+            height: auto;
+            padding: 10rem 0 15rem 0;
+        }
 
-h1, h2, h3, h4, h5, h6 {
-    font-weight: 500;
-    line-height: 1.2;
-    margin-top: 0;
-    margin-bottom: 1rem;
-    color: $body-color;
-}
+        .col-2, .col-2 > *, .col-3, .col-3 > *{
+            display: block;
+        }
 
-h1 {
-    font-size: 5.063rem;
-    margin-bottom: 0;
-}
-
-h2 {
-    font-size: 1.5rem;
-    @media #{$large-and-up} {
-        font-size: 3.375rem;
-    }
-}
-
-h3 {
-    font-size: 2.75rem;
-}
-
-h4 {
-    font-size: 2.25rem;
-    @media #{$small-and-down} {
-		font-size: 1.3rem;
-    }
-}
-
-h5 {
-    font-size: 1.833rem;
-    @media #{$small-and-down} {
-		font-size: 1.5rem;
-    }
-}
-
-h6 {
-    font-size: 1.222rem;
-    font-weight: bold;
-}
-
-small {
-    font-size: 0.815rem;
-}
-
-.fade-enter-active {
-    transition: opacity .5s
-}
-
-.fade-leave-active {
-    transition: opacity .5s
-}
-
-.wfs-loading {
-    visibility: hidden;
-}
-
-.wfs-active, .wf-inactive {
-    visibility: visible;
-}
-
-.card .content {
-    p {
-		font-size: 0.8rem;
-		line-height: 1.3rem;
-        margin-bottom: 0.8rem;
-	}
-}
-
-.card__learn-list,
-.card__activity-list,
-.activity-list-container {
-    font-family: $family-sanserif;
-}
-
-ol.resource__bullets {
-	>li {
-		ul, ol {
-			list-style-type: disc;
-			margin-left: 24px;
-            padding-left: 10px;
-            margin-top: 16px;
-            li:not(:last-child) {
-                margin-bottom: 8px;
-            }
-		}
-	}
-}
-
-.resource__takeaways {
-    h4 {
-        font-size: 1.3rem;
-        line-height: 1.8rem;
-        font-weight: bold;
-    }
-}
-
-.resource__excerpt p {
-	font-size: 1.2rem;
-	line-height: 1.8rem;
-}
-
-.case-study__context {
-    blockquote{
-        padding-left: 40px;
-        padding-right: 15%;
-        p {
-            font-family: $serif;
-            color: $green;
-            font-size: 1.3rem;
-            line-height: 1.8rem;
-            font-style: italic;
+        .col-2 > *, .col-3 > *{
+            height: 50%;
         }
     }
-}
 
-.quote {
-    max-width: 75%;
-    margin: 0 auto;
-    cite {
-        display: block;
-        text-align: right;
-        margin-top: 8px;
+
+    body{
+        font-family: "Comfortaa";
+        position: relative;
+        color: #d3d1cd;
+        background: url(assets/stars.jpg);
+        background-size: 25%;
     }
-}
 
-.full-width-quote {
-    font-family: $serif;
-    color: $green;
-    font-size: 1.5rem;
-    line-height: 2.2rem;
-    font-style: italic;
-    position: relative;
-    &:after {
-        content: '“';
+    a{
+        text-decoration: none;
+    }
+
+    a.but{
+        border-radius: 2rem;
+        border: 2px solid #e1ba44;
+        color: #e1ba44;
+        background: transparent;
+    }
+
+    a.but:hover{
+        background: #e1ba44;
+        color: #fff;
+        border: 2px solid #e1ba44;
+    }
+
+    section{
+        font-size: 2rem;
+    }
+
+    .heading{
+        font-size: 3rem;
+    }
+
+    .center{
+        width: 100%;
+        text-align: center;
+    }
+
+    .copy{
+        width: 70%;
+    }
+
+    .middle{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .full{
+        width: 100%;
+    }
+
+    .full > .fi{
+        width: 80%;
+        margin: auto;
+    }
+
+    /* ----- general blocks */
+
+    /* nav */
+
+    nav{
+        letter-spacing: 1.15;
+        font-size: 2vh;
         position: absolute;
-        top: 0;
-        left: -20px;
+        width: 100%;
+        z-index: 100;
+        font-family: "Dosis";
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
     }
-}
-
-// Grants
-
-.topContent {
-    margin: 32px 0;
-    font-size: 1rem;
-    @media #{$medium-and-up} {
-		 font-size: 1.2rem;
-    }
-    p {
-        font-size: 1rem;
-        line-height: 1.6;
-        @media #{$medium-and-up} {
-		 font-size: 1.2rem;
-         line-height: 1.5;
-        }
-    }
-    a {
-        border-bottom: 1px solid $blue;
-        &:hover {
-            color: $blue;
-            font-weight: bold;
-        }
-    }
-}
-
-.eligibility {
-    a {
-        border-bottom: 1px solid $blue;
-        &:hover {
-            color: $blue;
-            font-weight: bold;
-        }
-    }
-}
 
 
-.more-info {
-    // text-align: center;
-    margin-top: 50px;
-    margin-bottom: 50px;
-    @media #{$medium-and-down} {
-        padding: 0 24px;
-    }
-    @media #{$medium-and-up} {
-		margin-top: 150px;
-	}
-    @media #{$large-and-up} {
-		margin-top: 50px;
-	}
-    p, li {
-		font-size: 1rem;
-        line-height: 1.5;
-        @media #{$large-and-up} {
-			font-size: 1.1rem;
-            line-height: 1.7;
-		}
-    }
-    h4,h5 {
-        color: $orange;
-    }
-    h4 {
-        // margin-bottom: 8px;
-        line-height: 1.5;
-    }
-    h4 strong {
-        color: $orange !important;
-    }
-    h5 {
-        font-size: 1.2rem;
-        font-weight: bold;
-        line-height: 1.5;
-        @media #{$medium-and-up} {
-            font-size: 1.3rem;
-        }
-    }
-    ul {
-        margin-left: 48px;
-        list-style-type: disc;
-        li {
-            list-style-type: disc;
-            margin-bottom: 16px;
-        }
-    }
-    a {
-        border-bottom: 1px solid $blue;
-        &:hover {
-            color: $blue;
-            font-weight: bold;
-        }
-    }
-}
 
-.FAQ-content {
-	h2 {
-		font-size: 1.3rem;
-		line-height: 1.5;
-	}
-    p + h2 {
-        margin-top: 40px !important;
+    nav img{
+        position: relative;
+        height: 3.5vmin;
+        vertical-align: middle;
     }
-    span {
-        color: $off-black !important;
+    
+    .navinner{
+        display: flex;
+        align-items: center;
     }
-	ul {
-		margin: 20px 0 0 40px;
-		li {
-			line-height: 1.5;
-			margin-bottom: 1rem;
-			list-style-type: disc;
-			font-size: 1rem;
-			padding-left: .6em;
 
-		}
-	}
-}
-
-// Footer
-footer {
-    font-size: 0.8rem;
-    p, ul, li {
-        font-size: 0.8rem;
+    .navinner > *{
+        padding: 0.5vmin 1.5vmin;
+        display: flex;
+        align-items: center;
     }
-}
+
+    #ni2{
+        justify-content: flex-end;
+    }
+
+    #lang span{
+        padding: 0 0.25rem;
+    }
+
+    #lang span:not(.sel){
+        color: #aaa;
+    }
+
+    /* hero */
+    #banner{
+        position: absolute;
+        background: no-repeat bottom/100% url(assets/bannerbase.png);
+        bottom:0;
+        z-index: 50;
+    }
+    
+    #moon{
+        position: absolute;
+        width: 40rem;
+        right: 3rem;
+        top: 25%;
+        z-index: 50;
+    }
+    
+    #hero{
+        /* maybe make this fluid later */
+        margin-bottom: -10%;
+        background: no-repeat bottom/100% url(assets/bannerpeople.png);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        z-index: 100;
+        position: relative;
+        letter-spacing: 1.1;
+        height:100%;
+    }
+
+    .herosearch > input[type="text"]{
+        font-family: "Comfortaa";
+        width: 30rem;
+        margin-top: 2rem;
+        font-size: 2rem;
+        border:0;
+        background-color: rgba(255,255,255,0.1);
+        /*
+        background-image: url(https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-512.png);
+        background-size: 3.5rem;
+        background-position: right;
+        background-repeat: no-repeat;
+        */
+        padding: 1rem;
+        border-radius: 1rem;
+        color: #fff;
+    }
+
+    .herosearch > input[type="submit"]{
+        vertical-align: baseline;
+        transform: translate(-200%,0);
+        background: transparent;
+        border: 0;
+        margin: 0;
+        padding: 0;
+        font-size: 2rem;
+        font-family: sans-serif;
+        color: orangered;
+    }
+
+    .herosearch>*:focus{
+        outline: 0;
+        box-shadow: 0 0 2pt 0pt white;
+    }
+
+    #hero .blurb{
+        font-size: 6rem;
+        font-family: "Oswald";
+        text-transform: uppercase;
+        line-height: 1.1;
+        text-shadow: 1px 3px 1px rgba(255,255,255,0.2);
+    }
+
+    #hero .blurb p{
+        margin: 0;
+    }
+
+    /* map */
+
+    #map{
+        height: 100%;
+    }
+
+    /* media */
+
+    #media{
+        font-size: 0;
+    }
+
+    #media video{
+        width: 100%;
+        height: 100%;
+    }
+
+    /* howto */
+    /* tips */
+
+    #tips .col-3 > :first-child{
+        grid-column: left;
+    }
+
+    /* partners */
+
+    #partners{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    /* promo */
+
+    #promo > :first-child{
+        grid-column: left;
+    }
+
+    /* cta */
+
+    #cta{
+        height: 50%;
+    }
+
+    /* footer */
+    
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import { state, mutations } from './mutations'
-import { state } from './state'
-import { mutations } from './mutations'
-import { actions } from './actions'
-import { getters } from './getters'
-// import plugins from './plugins'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+
+const SET_PAGE = 'SET_PAGE';
+
 export default new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions
-//   plugins
+    state:{
+        debug: false,
+        count: 0,
+        page: null
+    },
+    mutations:{
+        SET_PAGE: (state, payload) => {
+            state.page = payload
+        },
+
+        increment (state) {
+            state.count++
+        }
+    },
+    actions:{
+        GET_PAGE: ({commit, dispatch}) => {
+            axios.get('http://park-pumpkin.test/cms/wp-json/wp/v2/pages/2')
+                .then(function (response) {
+                commit(SET_PAGE, response.data);
+            })
+                .catch(function (error) {
+                console.log(error)
+            })
+        }
+    }
 })
